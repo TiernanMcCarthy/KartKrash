1a345da963b9e58bba10cdf21cba5c1321495def
Tiernan McCarthy <tiernanmccarthy@outlook.com>
2025-09-13 16:50:13 +0100

    Cars driving improved, reverse and braking, better steering

---
+++ Assets/Scripts/Systems/UI/PlayerUI.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlayerUI : MonoBehaviour
+{
+    
+    
+    public static PlayerUI instance;
+    
+    
+    [SerializeField] TMP_Text carSpeedText;
+
+    private NetworkedVehicle playerVehicle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+    }
+
+    public void AssignVehicle(NetworkedVehicle playerVehicle)
+    {
+        this.playerVehicle = playerVehicle;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerVehicle != null)
+        {
+            carSpeedText.text = string.Format("{0} KPH", playerVehicle.GetCarSpeed().ToString("F2"));
+        }
+    }
+}
